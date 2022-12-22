0ee814cd66ef43025cde9ad4a40458b8f7f6e99d
amanda-benites <amandabenites2005@gmail.com>
2022-12-22 10:49:06 -0300

    testando mocks-stubs

--- cypress/e2e/3-ALURAPIC/login-mocks.cy.js
+++ cypress/e2e/3-ALURAPIC/login-mocks.cy.js
@@ -2,17 +2,15 @@ describe('Login usuarios alura pic', () => {
 
     beforeEach(() => {
         cy.visit('https://alura-fotos.herokuapp.com')
+
+        cy.intercept('POST', 'https://apialurapic.herokuapp.com/user/login', {
+            statusCode: 400
+        }).as('stubPost')
     })
 
     it('login correto', () => {
         cy.login(Cypress.env('userName'), Cypress.env('password'))
+        cy.wait('@stubPost')
         cy.contains('a', '(Logout)').should('be.visible');
-    })
-
-    it('login invalido', () => {
-        cy.login('ana', '321');
-        cy.on('window:alert', (str) => {
-            expect(str).to.equal('Invalid user name or password')
-        })
     })
 })
